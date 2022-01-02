6ab4eb5981e5bede36ca4628bca59cf243c6e10c
uic21796 <vivek.kumar@continental-corporation.com>
2022-01-02 20:01:00 +0530

    Std In and Out

--- src/hackerRank.java
+++ src/hackerRank.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class hackerRank {
     public static void helloWorld(){
-        System.out.println("Hellow World");
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
     }
+
+    public static void stdInOut(){
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+
+
 }
