b17445d3c71e48be1d5ad4e2f8cfeb5b6d731792
Dextr007 <vivek.kumar@continental-corporation.com>
2022-02-11 19:45:02 +0530

    Hacker Rank java to String

--- src/hackerRank.java
+++ src/hackerRank.java
@@ -70,7 +70,7 @@ public class hackerRank {
         }
         scan.close();
 
-
     }
+
 }
 
---
+++ src/toString.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class toString {
+    public static void solution(){
+        Scanner in = new Scanner(System.in);
+        int n = in .nextInt();
+        in.close();
+        //String s=???; Complete this line below
+        //Write your code here
+        String s = Integer.toString(n);
+
+        if (n == Integer.parseInt(s)) {
+            System.out.println("Good job");
+        } else {
+            System.out.println("Wrong answer.");
+        }
+
+    }
+
+}
+
